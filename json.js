ece2ab004ddd12f2466c96bf649868d722e18ba6
Usama Sultan <55051475+Usama-S@users.noreply.github.com>
2019-10-25 10:37:31 +0500

    Add files via upload

---
+++ json.js
@@ -0,0 +1,12 @@
+var key = [
+    { sr: "0", code: "0000", a: "hor-red", b: "vert-red", c: "vert-red", d: "hor-red", e: "vert-red", f: "vert-red", g: "hor" },
+    { sr: "1", code: "0001", a: "hor", b: "vert-red", c: "vert-red", d: "hor", e: "vert", f: "vert", g: "hor" },
+    { sr: "2", code: "0010", a: "hor-red", b: "vert-red", c: "vert", d: "hor-red", e: "vert-red", f: "vert", g: "hor-red" },
+    { sr: "3", code: "0011", a: "hor-red", b: "vert-red", c: "vert-red", d: "hor-red", e: "vert", f: "vert", g: "hor-red" },
+    { sr: "4", code: "0100", a: "hor", b: "vert-red", c: "vert-red", d: "hor", e: "vert", f: "vert-red", g: "hor-red" },
+    { sr: "5", code: "0101", a: "hor-red", b: "vert", c: "vert-red", d: "hor-red", e: "vert", f: "vert-red", g: "hor-red" },
+    { sr: "6", code: "0110", a: "hor-red", b: "vert", c: "vert-red", d: "hor-red", e: "vert-red", f: "vert-red", g: "hor-red" },
+    { sr: "7", code: "0111", a: "hor-red", b: "vert-red", c: "vert-red", d: "hor", e: "vert", f: "vert", g: "hor" },
+    { sr: "8", code: "1000", a: "hor-red", b: "vert-red", c: "vert-red", d: "hor-red", e: "vert-red", f: "vert-red", g: "hor-red" },
+    { sr: "9", code: "1001", a: "hor-red", b: "vert-red", c: "vert-red", d: "hor-red", e: "vert", f: "vert-red", g: "hor-red" },
+]
